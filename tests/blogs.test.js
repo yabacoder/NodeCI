579513f1c01fac76cc8e2724b9741b39e7c10cbf
Yabacoder <yabacoder@gmail.com>
2021-03-30 11:35:32 +0100

    fixed login test

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -82,7 +82,7 @@ describe("User is not logged in", async () => {
 		const results = await page.execRequests(actions);
 
 		for (let result of results) {
-			expect(result).toEqual({ error: "You must login" });
+			expect(result).toEqual({ error: "You must log in!" });
 		}
 	});
 
