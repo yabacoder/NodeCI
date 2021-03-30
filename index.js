89bf5f19941d40ecf636fbfe0ea0c306ac30cbd1
Yabacoder <yabacoder@gmail.com>
2021-03-30 10:46:21 +0100

    yml fix

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ if (["production", "ci"].includes(process.env.NODE_ENV)) {
 	});
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log(`Listening on port`, PORT);
 });
